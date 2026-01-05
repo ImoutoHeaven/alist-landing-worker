4dcb597c60bdcd0d58082205abc3e4ef4020993f
ImoutoHeaven <noreply@imoutoheaven.org>
2026-01-05 12:15:33 +0800

    Pin POW glue/esm URLs

--- snippets/src.js
+++ snippets/src.js
@@ -32,9 +32,9 @@ const DEFAULTS = {
   POW_COMMIT_COOKIE: "__Host-pow_commit",
   POW_SOL_COOKIE: "__Host-pow_sol",
   POW_ESM_URL:
-    "https://cdn.jsdelivr.net/gh/ImoutoHeaven/alist-landing-worker@cbdcae5dd7a8824b9922e4424026d9236f2d2a28/snippets/esm/esm.js",
+    "https://cdn.jsdelivr.net/gh/ImoutoHeaven/alist-landing-worker@30bac2d9e8532a1c27f81571c93235184cf00805/snippets/esm/esm.js",
   POW_GLUE_URL:
-    "https://cdn.jsdelivr.net/gh/ImoutoHeaven/alist-landing-worker@cbdcae5dd7a8824b9922e4424026d9236f2d2a28/snippets/glue.js",
+    "https://cdn.jsdelivr.net/gh/ImoutoHeaven/alist-landing-worker@30bac2d9e8532a1c27f81571c93235184cf00805/snippets/glue.js",
 };
 
 const CONFIG = [
